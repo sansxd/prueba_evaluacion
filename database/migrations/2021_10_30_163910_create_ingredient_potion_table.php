4c1b0b84c58f9ce7a1ec25e088db0e33aa0cbd73
sergio <sergioans95@hotmail.com>
2021-10-31 00:04:13 -0300

    create potion table and pivot table

---
+++ database/migrations/2021_10_30_163910_create_ingredient_potion_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngredientPotionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredient_potion', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ingredient_id')->constrained()->onDelete('cascade');
+            $table->foreignId('potion_id')->constrained()->onDelete('cascade');
+            $table->integer('amount');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ingredient_potion', function (Blueprint $table) {
+            //
+        });
+    }
+}
